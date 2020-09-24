425a665ebdc47a590f5e512b36d77c14eb688ce7
Daniel Toschläger <d.toschlaeger@gmail.com>
2020-09-24 23:05:01 +0200

    Update iacta.py

--- iacta.py
+++ iacta.py
@@ -6,7 +6,7 @@ from random import seed, randint
 while True:
     # 1. Frage "Wieviele Würfel"
     while True:
-        dice = input("\nWieviele Würfel (quit um zu beenden): ")
+        dice = input("\nWieviele Würfel (quit um zu beenden)? ")
         try:
             int_dice = int(dice)
             if int_dice < 1 or int_dice > 10:
@@ -24,7 +24,8 @@ while True:
 
     # 2. Frage "Was für ein Würfel Typ 1d10, 1d6, etc."
     while True:
-        dice_typ = input("Würfel Art: d")
+        dice_typ = input("Wieviele Augen soll der Würfel haben (quit um zu beenden)? ")
+        
         try:
             int_dice_typ = int(dice_typ)
             if int_dice_typ < 1 or int_dice_typ > 100:
@@ -43,30 +44,30 @@ while True:
     # User Wahl zeigen:
     print("Deine Wahl:", int_dice, "d", int_dice_typ)
 
-    # Counter
-    i = 1
-
-    # Initialize the random generator from OS specific random device
+    # den Random Generator mit /dev/urandom initialisieren wenn möglich
     while True:
         try:
-            # check if /dev/urandom is working
-            # If it exist (no error is produced) it is used to seed the python random generator later used
+            # Wenn das Laden des Moduls urandom keinen Fehler produziert, 
+            # weitermachen mit der Würfelsimulation
             from os import urandom
             seed(int.from_bytes(urandom(100), byteorder='big', signed=False))
-            print("Benutze /dev/urandom.")
+            #print("Benutze /dev/urandom.")
             break
         except ImportError:
-            # module not available, so use standard randint (should be also secure)
-            print("Benutze standard randint().")
+            # Modul urandom nicht vorhanden, darum eingebaute python random 
+            # Funktion benutzen um den Random Generator zu initialisieren
+            #print("Benutze standard randint().")
             seed(randint(1, 1000))
             break
         else:
-            # all other cases, kill -9 this script - bye bye ¯\_(ツ)_/¯ 
+            # Geht beides nich, auf wiedersehen! ¯\_(ツ)_/¯ 
             print("Kein random möglich. Bye!")
             raise SystemExit
 
-    # Eingabe auswerten und Würfe generieren:
+    # Würfe generieren:
+    i = 1
+
     while i <= int_dice:
         actual_throw = randint(1, int_dice_typ)
-        print("\nWurf", i, ":", actual_throw)
+        print("Wurf", i, ":", actual_throw)
         i = i + 1
